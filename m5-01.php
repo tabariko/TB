8cf5784e1f17f45093bdc751dad3cce2e7aad04f
tabariko <74866727+tabariko@users.noreply.github.com>
2020-11-23 00:08:17 +0900

    Update m5-01.php

--- m5-01.php
+++ m5-01.php
@@ -18,7 +18,7 @@
 
 	// DB接続設定
 	$dsn = 'データベース名';
-    $user = 'ユーザー名';
+    	$user = 'ユーザー名';
 	$password = 'パスワード';
 	$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	
